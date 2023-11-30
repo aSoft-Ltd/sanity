18e05fa49172a39726e89f5dfb5930f9c8ec6b55
andylamax <andylamax@programmer.net>
2023-11-30 03:31:49 +0300

    Made phone input searchable

--- remote/src/commonMain/kotlin/sanity/RemoteBus.kt
+++ remote/src/commonMain/kotlin/sanity/RemoteBus.kt
@@ -5,9 +5,7 @@ import kiota.EventSource
 
 fun RemoteBus(endpoint: SanityEndpoint): LocalBus {
     val bus = LocalBus()
-    println("Remote bus at: ${endpoint.events()}")
     EventSource(endpoint.events()).on("message") {
-        println("Received from")
         bus.dispatch(it.id ?: "*", it.data)
     }
     return bus
