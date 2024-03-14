09190202ac8323eea03ddbca3270624d20f640af
andylamax <andylamax@programmer.net>
2024-03-14 13:59:42 +0300

    Mergin with george

--- local/src/commonMain/kotlin/sanity/LocalBus.kt
+++ local/src/commonMain/kotlin/sanity/LocalBus.kt
@@ -3,7 +3,7 @@ package sanity
 import sanity.internal.AbstractDispatcher
 
 class LocalBus : AbstractDispatcher(), EventBus {
-    override fun subscribe(topic: String, callback: (Event) -> Unit): sanity.Subscriber {
+    override fun subscribe(topic: String, callback: (Event) -> Unit): Subscriber {
         val sub = Subscriber(topic, callback, subscribers)
         subscribers.add(sub)
         return sub
