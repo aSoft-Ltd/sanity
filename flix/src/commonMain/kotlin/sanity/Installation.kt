5fa435ae7430d7935c4684824fd97af29d52a68e
george <george.sechu@gmail.com>
2024-08-01 13:00:59 +0300

    pimonitor application status actions

--- flix/src/commonMain/kotlin/sanity/Installation.kt
+++ flix/src/commonMain/kotlin/sanity/Installation.kt
@@ -10,17 +10,16 @@ import io.ktor.server.response.respondBytesWriter
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
 import io.ktor.utils.io.writeStringUtf8
-import kotlinx.coroutines.awaitCancellation
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 fun Routing.installSanity(controller: SanityController) = get(controller.endpoint.events()) {
     call.response.cacheControl(CacheControl.NoCache(null))
     call.response.header(HttpHeaders.Connection, "Keep-Alive")
     call.respondBytesWriter(contentType = ContentType.Text.EventStream) {
+        val scope = CoroutineScope(Dispatchers.Default)
         // TODO: Subscriber gets treated harshly by the GC, find out why
         val subscriber = controller.handler.bus.subscribe("*") {
-            if (!isClosedForWrite) launch {
+            if (!isClosedForWrite) scope.launch {
                 writeStringUtf8("id: ${it.topic}\n")
                 writeStringUtf8("event: message\n")
                 writeStringUtf8("data: ${it.data}\n")
